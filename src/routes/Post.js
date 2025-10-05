1153261b76aa39e3b27f53bf95af3b65ab57f673
Rashmi8920 <rashmisingh25052002@gmail.com>
2025-10-05 20:11:38 +0530

    Update Post.js

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {createPostController, getAllPostsController,
+import {
+    createPostController,
+     getAllPostsController,
     getPostController,
-    // updatePostController, 
     getRelatedPostsController,
-    // delePostCoontroller,searchProductController
+    postFiltersController
+    // delePostCoontroller,searchProductController,  updatePostController
 } from '../controller/Post.js'
 
 const router= express.Router();
@@ -11,8 +13,10 @@ const router= express.Router();
 router.post("/create-post",createPostController)
 router.get("/get-post/:slug", getPostController);
 router.get("/get-all-post",getAllPostsController);
+router.get("/related-post/:postId/:categoryId",getRelatedPostsController);
+app.post("/product-filters", postFiltersController);
 // router.put("/update-post/:id",updatePostController);
 // router.delete("/delete-post/:id",delePostCoontroller);
-router.get("/related-post/:postId/:categoryId",getRelatedPostsController);
 // router.get('/search/:keyword',searchProductController)
+
  export default router;
