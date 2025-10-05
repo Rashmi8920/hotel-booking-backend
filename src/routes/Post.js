ac4d17d632870ec557da728c6b2848e50a2daf0e
Rashmi8920 <rashmisingh25052002@gmail.com>
2025-10-05 20:27:43 +0530

    Update Post.js

--- src/routes/Post.js
+++ src/routes/Post.js
@@ -14,10 +14,11 @@ router.post("/create-post",createPostController)
 router.get("/get-post/:slug", getPostController);
 router.get("/get-all-post",getAllPostsController);
 router.get("/related-post/:postId/:categoryId",getRelatedPostsController);
-app.post("/product-filters", postFiltersController);
+router.post("/product-filters", postFiltersController);
 // router.put("/update-post/:id",updatePostController);
 // router.delete("/delete-post/:id",delePostCoontroller);
 // router.get('/search/:keyword',searchProductController)
 
  export default router;
+
 
