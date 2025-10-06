7cc7fae167f1da3a0289b7e5d461d4537eea1bdf
Rashmi8920 <rashmisingh25052002@gmail.com>
2025-10-06 19:00:06 +0530

    Update Category.js

--- src/routes/Category.js
+++ src/routes/Category.js
@@ -1,15 +1,17 @@
 import express from "express";
-import { createCategoryController,getAllcategory,updateCategoryController,deleteCategory ,singleCategory} from "../controller/Category.js";
+import { createCategoryController,getAllcategory,updateCategoryController,deleteCategory ,singleCategory,selectedCategoryController} from "../controller/Category.js";
 import {isAdmin,requireSignIn} from '../middleware/Auth.js'
 
 const app =express.Router();
 
 app.post("/create-category",createCategoryController)
 app.get("/get-category",getAllcategory);
+app.get("/select-category/:slug", selectedCategoryController);
 app.put("/update-category/:id",
     // isAdmin,requireSignIn,
     updateCategoryController)
 app.delete("/delete-category/:id",deleteCategory)
 app.delete("/single-category/:slug",singleCategory)
+
 
 export default app;
